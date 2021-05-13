5b51112e4d5358f0f0f8473e4ed92cfee356de79
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-05-13 15:32:21 +0200

    Create test class for SimpleVirus class

---
+++ test_ps3b.py
@@ -0,0 +1,12 @@
+class TestSimpleVirus:
+    def test_get_max_birth_prob(self):
+        assert False
+
+    def test_get_clear_prob(self):
+        assert False
+
+    def test_does_clear(self):
+        assert False
+
+    def test_reproduce(self):
+        assert False
