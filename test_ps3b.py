a891da8139e1894c496624b083a3486db2e0b0c2
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-05-13 22:05:44 +0200

    Add TestPatients class with update method test

--- test_ps3b.py
+++ test_ps3b.py
@@ -1,7 +1,7 @@
 import pytest
 from assertpy import assert_that
 import random
-from ps3b import SimpleVirus, NoChildException
+from ps3b import SimpleVirus, NoChildException, Patient
 
 
 class TestSimpleVirus:
@@ -12,47 +12,78 @@ class TestSimpleVirus:
 
     @pytest.mark.parametrize('maxBirthProb, clearProb', [(0.1, 0.2)])
     def test_get_max_birth_prob(self):
-        #Given
+        # Given
         virus = self.simple_virus
-        #When
+        # When
         result = virus.getMaxBirthProb()
-        #Then
+        # Then
         assert_that(result).is_equal_to(0.1)
 
     @pytest.mark.parametrize('maxBirthProb, clearProb', [(0.1, 0.3)])
     def test_get_clear_prob(self):
-        #Given
+        # Given
         virus = self.simple_virus
-        #When
+        # When
         result = virus.getClearProb()
-        #Then
+        # Then
         assert_that(result).is_equal_to(0.3)
 
-    @pytest.mark.parametrize('maxBirthProb, clearProb, expected', [(0.1, 0.8, True), (0.1, 0.6, True), (0.1, 0.3, False)])
+    @pytest.mark.parametrize(
+        'maxBirthProb, clearProb, expected',
+        [(0.1, 0.8, True), (0.1, 0.6, True), (0.1, 0.3, False)])
     def test_does_clear(self, expected):
-        #Given
+        # Given
         virus = self.simple_virus
-        #When
+        # When
         result = virus.doesClear()
-        #Then
+        # Then
         print(result)
         assert_that(result).is_equal_to(expected)
 
-
-    @pytest.mark.parametrize('maxBirthProb, clearProb, popDensity', [(0.9, 0.8, 0.2), (0.8, 0.6, 0.3)])
+    @pytest.mark.parametrize('maxBirthProb, clearProb, popDensity',
+                             [(0.9, 0.8, 0.2), (0.8, 0.6, 0.3)])
     def test_reproduce(self, popDensity):
-        #Given
+        # Given
         virus = self.simple_virus
-        #When
+        # When
         result = virus.reproduce(popDensity)
-        #Then
+        # Then
         assert_that(result).is_instance_of(SimpleVirus)
 
-    @pytest.mark.parametrize('maxBirthProb, clearProb, popDensity', [(0.2, 0.8, 0.2), (0.1, 0.6, 0.3)])
+    @pytest.mark.parametrize('maxBirthProb, clearProb, popDensity',
+                             [(0.2, 0.8, 0.2), (0.1, 0.6, 0.3)])
     def test_reproduce(self, popDensity):
-        #Given
+        # Given
         virus = self.simple_virus
-        #When
+        # When
         result = virus.reproduce
-        #Then
+        # Then
         assert_that(result).raises(NoChildException).when_called_with(popDensity)
+
+
+class TestPatient:
+
+    @pytest.fixture(autouse=True)
+    def prepare_patient(self, viruses, maxPop):
+        self.patient = Patient(viruses, maxPop)
+
+    def test_get_viruses(self):
+        assert False
+
+    def test_get_max_pop(self):
+        assert False
+
+    def test_get_total_pop(self):
+        assert False
+
+    @pytest.mark.parametrize('viruses, maxPop, expected',
+                             [([SimpleVirus(1.0, 1.0)], 100, 0),
+                              ([SimpleVirus(0.04, 0.07), SimpleVirus(0.12, 0.16)], 6, 2)])
+    def test_update(self, viruses, maxPop, expected):
+        # Given
+        patient = self.patient
+        # When
+        patient.update()
+        # Then
+        result = patient.getTotalPop()
+        assert_that(result).is_equal_to(expected)
