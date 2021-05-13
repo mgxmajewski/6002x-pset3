95a8ca890f41c1823bc2d5a3a54c613c88ee7bb1
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-05-13 17:28:02 +0200

    Fix fixture to prepare SimpleVirus

--- test_ps3b.py
+++ test_ps3b.py
@@ -6,25 +6,18 @@ from ps3b import SimpleVirus
 
 class TestSimpleVirus:
 
-    # @pytest.fixture(autouse=True)
-    # def prepare_simple_virus(self, maxBirthProb, clearProb):
-    #     # random.seed(1)
-    #
-    #     # self.clearProb = clearProb
-    #     # maxBirthProb = None
-    #     # clearProb = None
-    #     return SimpleVirus(maxBirthProb.param, clearProb.param)
+    @pytest.fixture(autouse=True)
+    def prepare_simple_virus(self, maxBirthProb, clearProb):
+        self.simple_virus = SimpleVirus(maxBirthProb, clearProb)
 
-    @pytest.mark.parametrize('maxBirthProb, clearProb', [(0.1, 0.2), (0.2, 0.2)])
+    @pytest.mark.parametrize('maxBirthProb, clearProb', [(0.1, 0.2)])
     def test_get_max_birth_prob(self, maxBirthProb, clearProb):
         #Given
-        # self.maxBirthProb = maxBirthProb
-        # self.clearProb = clearProb
-        virus = SimpleVirus(maxBirthProb, clearProb)
+        virus = self.simple_virus
         #When
         result = virus.getMaxBirthProb()
         #Then
-        assert_that(result).is_equal_to(0.1)
+        assert_that(result).is_close_to(0.1, 3)
 
     def test_get_clear_prob(self):
         assert False
