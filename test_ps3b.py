25494c0dcb2afa3e6675f057c37e6eb0296475a6
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-05-13 17:16:52 +0200

    Parametrize test_get_max_birth_prob

--- test_ps3b.py
+++ test_ps3b.py
@@ -1,6 +1,30 @@
+import pytest
+from assertpy import assert_that
+import random
+from ps3b import SimpleVirus
+
+
 class TestSimpleVirus:
-    def test_get_max_birth_prob(self):
-        assert False
+
+    # @pytest.fixture(autouse=True)
+    # def prepare_simple_virus(self, maxBirthProb, clearProb):
+    #     # random.seed(1)
+    #
+    #     # self.clearProb = clearProb
+    #     # maxBirthProb = None
+    #     # clearProb = None
+    #     return SimpleVirus(maxBirthProb.param, clearProb.param)
+
+    @pytest.mark.parametrize('maxBirthProb, clearProb', [(0.1, 0.2), (0.2, 0.2)])
+    def test_get_max_birth_prob(self, maxBirthProb, clearProb):
+        #Given
+        # self.maxBirthProb = maxBirthProb
+        # self.clearProb = clearProb
+        virus = SimpleVirus(maxBirthProb, clearProb)
+        #When
+        result = virus.getMaxBirthProb()
+        #Then
+        assert_that(result).is_equal_to(0.1)
 
     def test_get_clear_prob(self):
         assert False
